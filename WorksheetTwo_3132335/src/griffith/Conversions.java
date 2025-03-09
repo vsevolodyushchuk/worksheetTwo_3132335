54b9c3718483a3db3b8eda9223f124a82266f736
Vsevolod Yushchuk <Sevayushchuk@gmail.com>
2025-03-09 12:31:11 +0000

    Part two

--- WorksheetTwo_3132335/src/griffith/Conversions.java
+++ WorksheetTwo_3132335/src/griffith/Conversions.java
@@ -2,4 +2,29 @@ package griffith;
 
 public class Conversions {
 
+    public double euroToDollar(double euro) {
+        // Method stub
+        return 0.0;
+    }
+
+    public double dollarToEuro(double dollar) {
+        // Method stub
+        return 0.0;
+    }
+
+    public int stringToInteger(String val) {
+        // Method stub
+        return 0;
+    }
+
+    public String integerToString(int val) {
+        // Method stub
+        return "";
+    }
+
+    public String switchCase() {
+        // Method stub
+        return "";
+    }
 }
+
