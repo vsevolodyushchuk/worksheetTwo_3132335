93f43cf4c770dfde4083375b56f0267d7cde081d
Vsevolod Yushchuk <Sevayushchuk@gmail.com>
2025-03-09 13:53:01 +0000

    DollarToEuro test 1

--- WorksheetTwo_3132335/test/griffith/ConversionsTest.java
+++ WorksheetTwo_3132335/test/griffith/ConversionsTest.java
@@ -14,6 +14,13 @@ class ConversionsTest {
 	    assertEquals(110.0, conversions.euroToDollar(100), 0.01);
 	    assertEquals(0.0, conversions.euroToDollar(0), 0.01);
 	}
-
+	
+	@Test
+	void testDollarToEuro() {
+	    Conversions conversions = new Conversions();
+	    
+	    assertEquals(0.91, conversions.dollarToEuro(1), 0.01);
+	    
+	}
 
 }
